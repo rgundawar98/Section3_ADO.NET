057b66d070cac6c67dee1b4c80ea8a93065ee03e
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-20 16:50:55 +0530

    Adding welcome message

---
+++ ADO.NETProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.NETProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the ADO.NET Problems");
+        }
+    }
+}
